342129433ddeb7cdc291e5232ec4e940d9d744c5
Keisha Achmad Fiandika Putera <kaitzyroslok@gmail.com>
2026-09-23 21:02:38 +0700

    Delete Praktik directory

--- Praktik/praktik_modul.py
+++
@@ -1,5 +0,0 @@
-riwayat_pencarian = ["jadwal sholat", "gempa bumi", "cuaca hari ini", "Manchester United", "KAI recruitment", "wallpaper keren", "berita terkini", "spotify", "redbus", "real madrid"]
-
-riwayat_pencarian.append("super league")
-
-print(riwayat_pencarian)
